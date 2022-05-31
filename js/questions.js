61ff232892a1805b424118ac23726a2618ad9a23
Aibal jose <69980574+aibaljose@users.noreply.github.com>
2022-05-31 19:09:04 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,135 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following cannot be classified as an auxiliary to trade?",
+    answer: "Mining",
+    options: [
+      "Mining",
+      "Insurance",
+      "Warehousing",
+      "Transport"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following cannot be classified as an objective of business?",
+    answer: "Investment",
+    options: [
+      "Investment",
+      "Productivity",
+      "Innovation",
+      "Profit earning"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The industries which provide support services to other industries are known as",
+    answer: "Tertiary industries.",
+    options: [
+      "Primary industries",
+      "Secondary industries",
+      "Commercial industries",
+      "Tertiary industries."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the broad categories of industries covers oil refinery and sugar mills?",
+    answer: "Secondary",
+    options: [
+      "Primary",
+      "Secondary",
+      "Tertiary",
+      "None of them"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Business risk is not likely to arise due to ?",
+    answer: "Good management",
+    options: [
+      "Changes in government policy",
+      "Good management",
+      "Employee dishonesty",
+      "Power failure"
+    ]
+    
+  },
+  {
+ numb: 6,
+ question: "Find the odd one",
+ answer: "Praying in a church",
+ options: [
+    "Teaching in a school",
+    "Practicing in a court",
+    "Praying in a church",
+    "Running a hotel"
+  ]  
+
+  },
+  
+    {
+ numb: 7,
+ question: "The occupation in which people work for others and get remunerated in return is known as",
+ answer: "Employment",
+ options: [
+    "Business",
+    "Employment",
+    "Profession",
+    "None of them"
+  ]  
+
+  },
+    {
+ numb: 8,
+ question: "Which of the following does not characterize business activity?",
+ answer: "Salary or wages",
+ options: [
+    "Production of goods and services",
+    "Presence of risk",
+    "Sale or exchange of goods and services",
+    "Salary or wages"
+  ]  
+
+  },
+      {
+ numb: 9,
+ question: "Complete the following equation: Commerce = ____+ Aids to trade",
+ answer: "Trade",
+ options: [
+    "Service",
+    "Industry",
+    "Business",
+    "Trade"
+  ]  
+
+  },
+      {
+ numb: 10,
+ question: "Extraction of coal is an example of industries.",
+ answer: "",
+ options: [
+    "Genetic",
+    "Construction",
+    "Extractive",
+    "Manufacturing"
+  ]  
+
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   //  {
+    // numb: 7,
+   //  question: "Your Question is Here",
+  //  answer: "Correct answer of the question is here",
+   //  options: [
+   //    "Option 1",
+    //   "option 2",
+    //  "option 3",
+    //   "option 4"
+   //  ]
+   //},
+];
