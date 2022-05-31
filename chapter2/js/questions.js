a90ea614615a10ff001374b0faf688a12d753191
Aibal jose <69980574+aibaljose@users.noreply.github.com>
2022-05-31 19:33:02 +0530

    Add files via upload

---
+++ chapter2/js/questions.js
@@ -0,0 +1,135 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Profits do not have to be shared. This statement refers to ………?",
+    answer: "Sole proprietorship",
+    options: [
+      "Partnership",
+      "Joint Hindu family business",
+      "Sole proprietorship",
+      "Company"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The maximum number of partners allowed in the banking business is:?",
+    answer: "Ten",
+    options: [
+      "Twenty",
+      "Ten",
+      "No limit",
+      "Two"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The structure in which there is a separation of ownership and management is called",
+    answer: "Company",
+    options: [
+      "Sole proprietorship",
+      "Partnership",
+      "Company",
+      "All business"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The minimum number of partners required to form a partnership business is?",
+    answer: "Two",
+    options: [
+      "Twenty",
+      "Ten",
+      "No limit",
+      "Two"
+    ]
+  },
+    {
+    numb: 5,
+    question: " partner whose association with the firm is unknown to the general public is called ?",
+    answer: "Secret partner",
+    options: [
+      "Active partner",
+      " Sleeping partner",
+      "Nominal partner",
+      "Secret partner"
+    ]
+    
+  },
+  {
+ numb: 6,
+ question: "The Head of the joint Hindu family business is called",
+ answer: "Karta",
+ options: [
+    "Proprietor",
+    "Director",
+    "Karta",
+    "Manager"
+  ]  
+
+  },
+  
+    {
+ numb: 7,
+ question: "The Karta in Joint Hindu family business has ",
+ answer: "Unlimited liability",
+ options: [
+    "Limited liability",
+    "Unlimited liability",
+    "No liability for debts",
+    "Joint liability"
+  ]  
+
+  },
+    {
+ numb: 8,
+ question: "In a Co-operative society, the principle followed is ",
+ answer: "One man one vote",
+ options: [
+    "Salary or wages",
+    "One man one vote",
+    "No vote",
+    "Multiple votes"
+  ]  
+
+  },
+      {
+ numb: 9,
+ question: "The capital of a company is divided into a number of parts each one of which is called …………",
+ answer: "Share",
+ options: [
+    "Profit",
+    "Dividend",
+    "Interest",
+    "Share"
+  ]  
+
+  },
+      {
+ numb: 10,
+ question: "The liability of a sole trader is ………",
+ answer: "",
+ options: [
+    "Unlimited",
+    "limited",
+    "No liability",
+    "none"
+  ]  
+
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   //  {
+    // numb: 7,
+   //  question: "Your Question is Here",
+  //  answer: "Correct answer of the question is here",
+   //  options: [
+   //    "Option 1",
+    //   "option 2",
+    //  "option 3",
+    //   "option 4"
+   //  ]
+   //},
+];
